d565a82362d1a05f986cdedecff16f1a72e4ff8f
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-11-20 09:55:54 -0800

    styled the labels  in the signin page

--- client/src/Signin.js
+++ client/src/Signin.js
@@ -19,29 +19,33 @@ class Signin extends React.Component {
   render() {
     return (
       <form className="signin-form">
-        <h2 className = "signin-form__title">SIGN IN</h2>
+        <h2 className="signin-form__title">SIGN IN</h2>
 
-        <label>Email</label>
+        <label className="form__label-email">Email</label>
         <input
           type="text"
           name="email"
           onChange={this.updateEmail}
           value={this.state.email}
-          className ="form__name-input"
-          placeholder ="Enter your email"
+          className="form__name-input"
+          placeholder="Enter your email"
         />
 
-        <label>Password</label>
+        <label className="form__label-password">Password</label>
         <input
           type="password"
           name="password"
           onChange={this.updatePassword}
           value={this.state.password}
-          className = "form__password-input"
-          placeholder ="Enter your password"
+          className="form__password-input"
+          placeholder="Enter your password"
         />
 
-        <button type="button" onClick={this.handleClick} className = "form__submit-button">
+        <button
+          type="button"
+          onClick={this.handleClick}
+          className="form__submit-button"
+        >
           Sign In
         </button>
       </form>
