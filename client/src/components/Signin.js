d81d6bbe61586153bcffae680e5695d7b8a7b35a
Ivan Yang <0201ivan@gmail.com>
2020-11-20 15:03:39 -0800

    modified state data

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,13 +1,17 @@
 import React from "react";
 import axios from "axios";
+import Profile from "./Profile";
+
 const main_url = "http://localhost:5000";
 
 class Signin extends React.Component {
   state = {
+    data: {},
     email: "",
     password: "",
-    signedIn:false
+    signedIn:false,
   };
+
 
   updateEmail = (e) => {
     this.setState({ email: e.target.value });
@@ -16,7 +20,7 @@ class Signin extends React.Component {
     this.setState({ password: e.target.value });
   };
   handleClick = () => {
-     if(this.state.emain && this.state.password) {
+     if(this.state.email && this.state.password) {
          axios.post(main_url + "/signin")
          .then(response =>{
              console.log(response);
@@ -35,38 +39,43 @@ class Signin extends React.Component {
 
   render() {
     return (
-      <form className="signin-form">
-        <h2 className="signin-form__title">SIGN IN</h2>
+      (!this.state.signedIn)?
+        <form className="signin-form">
+          <h2 className="signin-form__title">SIGN IN</h2>
 
-        <label className="form__label-email">Email:</label>
-        <input
-          type="text"
-          name="email"
-          onChange={this.updateEmail}
-          value={this.state.email}
-          className="form__name-input"
-          placeholder="Enter your email"
-        />
+          <label className="form__label-email">Email:</label>
+          <input
+            type="text"
+            name="email"
+            onChange={this.updateEmail}
+            value={this.state.email}
+            className="form__name-input"
+            defaultValue="Enter your email"
+          />
 
-        <label className="form__label-password">Password:</label>
-        <input
-          type="password"
-          name="password"
-          onChange={this.updatePassword}
-          value={this.state.password}
-          className="form__password-input"
-          placeholder="Enter your password"
-        />
+          <label className="form__label-password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={this.updatePassword}
+            value={this.state.password}
+            className="form__password-input"
+            defaultValue="Enter your password"
+          />
 
-        <button
-          type="button"
-          onClick={this.handleClick}
-          className="form__submit-button"
-        >
-          Sign In
-        </button>
-      </form>
-    );
+          <button
+            type="button"
+            onClick={this.handleClick}
+            className="form__submit-button"
+          >
+            Sign In
+          </button>
+        </form>
+        :
+        <Profile data={this.state.data} />
+    
+        )
+      
   }
 }
 
